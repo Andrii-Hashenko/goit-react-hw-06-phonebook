7ad02ff91289e5a8c4776cac2752a60f56c64f78
Andrii Hashenko <hashenkoaa@gmail.com>
2023-06-25 10:57:42 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,38 @@
 import Form from './Form';
 import ContactList from './ContactList';
+import Spinner from './Spinner/Spinner';
 import Filter from './Filter';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './container.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { addItems, deleteItems } from '../redux/itemsSlice';
 import { updateFilter } from '../redux/filterSlice';
+import {
+  useFetchContactsQuery,
+  useDeleteContactsMutation,
+  useCreateContactsMutation,
+} from 'redux/itemsSlice';
 
 export default function App() {
-  const { items } = useSelector(state => state.items);
-
   const value = useSelector(state => state.filter);
-
   const dispatch = useDispatch();
+
+  const { data, isFetching } = useFetchContactsQuery();
+  const [deleteItem] = useDeleteContactsMutation();
+  const [newContact] = useCreateContactsMutation();
+
+  const addContact = ({ name, phone }) => {
+    const contact = {
+      name,
+      phone,
+    };
+
+    if (data.some(contact => contact.name === name)) {
+      return toast.info(`${contact.name}   is   already in contacts`);
+    }
+
+    newContact(contact);
+  };
 
   const changeFilter = e => {
     dispatch(updateFilter(e.currentTarget.value));
@@ -19,30 +40,31 @@ export default function App() {
 
   const getVisibleContacts = () => {
     const normalizedFilter = value.toLowerCase();
-
-    return items.filter(contact =>
+    return data.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
-  };
-
-  const deleteContact = contactId => {
-    dispatch(deleteItems(contactId));
-
-    dispatch(updateFilter(''));
   };
 
   return (
     <div className="Container">
       <h1>Phonebook</h1>
-      {}
-      <Form onSubmit={contact => dispatch(addItems(contact))} />
-      <h2> Contacts : </h2>
-      {}
-      <Filter value={value} onChange={changeFilter} />
-      <ContactList
-        contacts={getVisibleContacts()}
-        onDeleteContactList={deleteContact}
+      <Form onSubmit={addContact} />
+      <ToastContainer
+        position={'top-center'}
+        autoClose={3000}
+        theme={'colored'}
       />
+      <h2> Contacts : </h2>
+      <Filter value={value} onChange={changeFilter} />
+      {}
+      {isFetching && <Spinner />}
+      {}
+      {data && (
+        <ContactList
+          contacts={getVisibleContacts()}
+          onDeleteContact={deleteItem}
+        />
+      )}
     </div>
   );
 }
